9d1f1e09704fa5f793dfd883217bdb440aa8d0aa
eunchan96 <taegwon02@gmail.com>
2025-07-03 12:24:12 +0900

    Member init

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,13 +1,17 @@
 package com.back.domain.post.post.entity;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static jakarta.persistence.CascadeType.PERSIST;
 import static jakarta.persistence.CascadeType.REMOVE;
@@ -16,13 +20,15 @@ import static jakarta.persistence.CascadeType.REMOVE;
 @Getter
 @NoArgsConstructor
 public class Post extends BaseEntity {
+    @ManyToOne
+    private Member author;
     private String title;
     private String content;
 
     @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
     private List<PostComment> comments = new ArrayList<>();
 
-    public Post(String title, String content) {
+    public Post(Member author, String title, String content) {
         this.title = title;
         this.content = content;
     }
@@ -32,8 +38,8 @@ public class Post extends BaseEntity {
         this.content = content;
     }
 
-    public PostComment addComment(String content) {
-        PostComment postComment = new PostComment(this, content);
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(author, this, content);
         comments.add(postComment);
 
         return postComment;
