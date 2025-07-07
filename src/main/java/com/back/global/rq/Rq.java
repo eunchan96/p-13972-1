3d4911dfb70afe85f1fb164dd1e9ca0c7a3b1bc4
eunchan96 <taegwon02@gmail.com>
2025-07-07 14:16:54 +0900

    cookie

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -3,7 +3,9 @@ package com.back.global.rq;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import com.back.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class Rq {
     private final MemberService memberService;
     private final HttpServletRequest req;
+    private final HttpServletResponse resp;
 
     public Member getActor() {
         String headerAuthorization = req.getHeader("Authorization");
@@ -26,5 +29,9 @@ public class Rq {
 
         return memberService.findByApiKey(apiKey)
                 .orElseThrow(() -> new ServiceException("401-3", "API Key가 유효하지 않습니다."));
+    }
+
+    public void setCookie(String name, String value) {
+        resp.addCookie(new Cookie(name, value));
     }
 }
