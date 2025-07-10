629483b3e40f92791b6a69834be35286d20b123a
eunchan96 <taegwon02@gmail.com>
2025-07-10 15:47:15 +0900

    requestMatchers

--- src/main/java/com/back/global/security/SecurityConfig.java
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.back.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -20,8 +21,11 @@ public class SecurityConfig {
                         auth -> auth
                                 .requestMatchers("/favicon.ico").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/posts", "/api/*/posts/{id:\\d+}", "/api/*/posts/{id:\\d+}/comments", "/api/*/posts/{id:\\d+}/comments/{id:\\d+}").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/members/login", "/api/*/members/logout").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/*/members").permitAll()
                                 .requestMatchers("/api/*/adm/**").hasRole("ADMIN")
-                                .requestMatchers("/**").permitAll()
+                                .requestMatchers("/api/*/**").authenticated()
                                 .anyRequest().authenticated()
                 )
                 .headers(
