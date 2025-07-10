1e35d64fee1e73f6e7543953b091de6f85d06e98
eunchan96 <taegwon02@gmail.com>
2025-07-10 15:52:22 +0900

    requestMatchers (수정)

--- src/main/java/com/back/global/security/SecurityConfig.java
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                                 .requestMatchers("/favicon.ico").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/*/posts", "/api/*/posts/{id:\\d+}", "/api/*/posts/{postId:\\d+}/comments", "/api/*/posts/{postId:\\d+}/comments/{id:\\d+}").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/*/members/login", "/api/*/members/logout").permitAll()
+                                .requestMatchers("/api/*/members/login", "/api/*/members/logout").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/*/members").permitAll()
                                 .requestMatchers("/api/*/adm/**").hasRole("ADMIN")
                                 .requestMatchers("/api/*/**").authenticated()
