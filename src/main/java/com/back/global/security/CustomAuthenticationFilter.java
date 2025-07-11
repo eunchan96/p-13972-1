ee55e8db2bd6c8b4035a48f7ad9d74425735219d
eunchan96 <taegwon02@gmail.com>
2025-07-11 11:32:18 +0900

    member.getAuthorities()

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -14,15 +14,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -119,15 +116,12 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             rq.setHeader("Authorization", actorAccessToken);
         }
 
-        Collection<? extends GrantedAuthority> authorities = member.isAdmin() ?
-                List.of(new SimpleGrantedAuthority("ROLE_ADMIN")) : List.of();
-
         UserDetails user = new SecurityUser(
                 member.getId(),
                 member.getUsername(),
                 "",
                 member.getName(),
-                authorities
+                member.getAuthorities()
         );
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(
